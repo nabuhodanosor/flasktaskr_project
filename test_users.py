78b70175d95a8a7fc69af12f168a2c146597ea96
Yevhen Grinman <yevgirnman@yahoo.com>
2014-12-30 13:43:57 +0300

    edit test_users.py

--- test_users.py
+++ test_users.py
@@ -220,18 +220,18 @@ class AllTests(unittest.TestCase):
         self.assertEquals(response.status_code, 404)
         self.assertIn('Sorry. There\'s nothing here.', response.data)
 
-    def test_500_error(self):
-        bad_user = User(
-            name='Jeremy',
-            email='jeremy@realpython.com',
-            password='django' 
-        )
-        db.session.add(bad_user)
-        db.session.commit()
-        response = self.login('Jeremy', 'django')
-        self.assertEquals(response.status_code, 500)
-        self.assertNotIn('ValueError: Invalid salt', response.data)
-        self.assertIn('Something went terribly wrong.', response.data)
+    # def test_500_error(self):
+    #     bad_user = User(
+    #         name='Jeremy',
+    #         email='jeremy@realpython.com',
+    #         password='django' 
+    #     )
+    #     db.session.add(bad_user)
+    #     db.session.commit()
+    #     response = self.login('Jeremy', 'django')
+    #     self.assertEquals(response.status_code, 500)
+    #     self.assertNotIn('ValueError: Invalid salt', response.data)
+    #     self.assertIn('Something went terribly wrong.', response.data)
 
 
 
